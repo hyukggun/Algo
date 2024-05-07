f1defc594c05a55b58719b0f407de059c5fa0d18
hyukggunchoe <hyukggunchoe@gmai.com>
2024-05-07 21:00:34 +0900

    🗑️ Contents.swift

--- Algo.playground/Contents.swift
+++ Algo.playground/Contents.swift
@@ -1,11 +1,3 @@
 import Foundation
 import UIKit
 
-let a = Int("a".unicodeScalars.first!.value)
-
-
-let str = "abcedf"
-
-let sol = Solution2370()
-let ans = sol.longestIdealString("azbzczdzefzzqeufz", 2)
-print(ans)
