5aa3ce31296e54fb50d6fe4d0aef9d80f8c53c72
hyukggunchoe <hyukggunchoe@gmai.com>
2024-05-13 21:13:06 +0900

    Solve 786, but slow

--- Algo.playground/Sources/786.swift
+++ Algo.playground/Sources/786.swift
@@ -1,2 +1,38 @@
 import Foundation
 
+/*
+ 출처: https://logh.tistory.com/16 [LogH:티스토리]
+ 풀이방법:
+ 1. heap 을 이용한 풀이 O(N^2 * logN)
+ 2. Binary Search 를 이용한 풀이
+*/
+class Solution786 {
+    static func kthSmallestPrimeFraction(_ arr: [Int], _ k: Int) -> [Int] {
+        var heap = Heap<[Int]>(compareFunc: { left, right in
+            let lValue = Double(left[0]) / Double(left[1])
+            let rValue = Double(right[0]) / Double(right[1])
+            return lValue < rValue
+        })
+
+        var fractions = [[Int]]()
+        let n = arr.count - 1
+        for i in stride(from: n, to: -1, by: -1) {
+            for j in 0..<i {
+                fractions.append([arr[j], arr[i]])
+            }
+        }
+
+        var t = 0
+        heap.container.append(contentsOf: fractions)
+        heap.makeHeap()
+
+        var ans: [[Int]] = []
+        while !heap.isEmpty && t < k {
+            ans.append(heap.pop()!)
+            t += 1
+        }
+
+        return ans.last!
+    }
+}
+
